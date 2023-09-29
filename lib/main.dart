d06fed071045ad04efc33bd3c38be817d0890348
mohamedSayedBayoumy <mandobobo12345@gmail.com>
2023-09-29 23:13:24 +0300

    close LandScape Mode ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 
 import 'home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ], //هنا ان بحط الشكل الي انا عايزه يظهر بس في التطبيق
+  );
   runApp(
     DevicePreview(
       builder: (context) => const MyApp(),
